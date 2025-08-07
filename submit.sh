92b95331224f0c4fe73ba3c7942c72419b96394d
Iseult Leahy <iseult.leahy@crg.eu>
2025-08-07 16:10:57 +0100

    increase time

--- submit.sh
+++ submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --no-requeue
 #SBATCH --mem 16G
 #SBATCH -p genoa64
-#SBATCH --time 00:10:00
+#SBATCH --time 02:00:00
 #SBATCH --output=/no_backup/rg/ileahy/logs/nf_secis_independent_horse_%A.out  # log per job
 #SBATCH --error=/no_backup/rg/ileahy/logs/nf_secis_independent_horse_%A.err   # error log
  
