cbb9eefcb888f6859d46a8bd60c8ea108d9a15fd
Iseult Leahy <iseult.leahy@crg.eu>
2025-08-08 14:31:27 +0100

    1

--- bin/select_interesting_recodings.py
+++ bin/select_interesting_recodings.py
@@ -28,10 +28,16 @@ def process_recoding_predictions(geneid_txt):
     out_of_frame = 0
     
     def get_transcript_name(seq_name):
-        return re.search(r'^(.*?)_TGA', seq_name).group(1)
+        if "_TGA" not in seq_name:
+            return None
+        else:
+            return re.search(r'^(.*?)_TGA', seq_name).group(1)
     
     def get_gene_name(seq_name):
-        return re.search(r'^(.*?)_TGA', seq_name).group(1).split('-')[0]
+        if "_TGA" not in seq_name:
+            return None
+        else:
+            return re.search(r'^(.*?)_TGA', seq_name).group(1).split('-')[0]
 
     with open(geneid_txt) as f:
         current_seq = None
