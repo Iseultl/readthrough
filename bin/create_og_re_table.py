51029a588322c1877745b877809d6f724e31017d
Iseult Leahy <iseult.leahy@crg.eu>
2025-08-11 12:48:32 +0100

    1

--- bin/create_og_re_table.py
+++ bin/create_og_re_table.py
@@ -84,8 +84,9 @@ def read_gff(gff):
         names=['chr', 'source', 'type', 'start', 'end', 'score', 'strand', 'phase', 'attributes']
     )
      
-    df[['gene_name', 'transcript_name']] = df['attributes'].str.split('_', n=1, expand=True)
-
+    df['gene_name'] = df['attributes'].str.split('_', n=1, expand=True)[0]
+    df['transcript_name'] = df['attributes'].str.split('_', n=1, expand=True)[1]
+    
     # Filter for CDS
     cds_df = df[df['type'] == 'CDS']
 
