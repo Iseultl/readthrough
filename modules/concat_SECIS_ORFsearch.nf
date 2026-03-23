28f147212ccb3b40f812b460946d31662ed0f438
Iseult Leahy <iseult.leahy@crg.eu>
2026-03-23 11:09:42 +0100

    run on Babesia

--- modules/concat_SECIS_ORFsearch.nf
+++ modules/concat_SECIS_ORFsearch.nf
@@ -15,9 +15,9 @@ process concat_SECIS_ORFsearch {
     """
     # Python script to concatenate the results 
     add_SECIS_annotation.py \\
-        --orfsearch ${orfsearch_result} \\
-        --secis_gff ${merged_secis_gff} \\
-        --filtered_sec ${filtered_secis} \\
+        --ORFsearch ${orfsearch_result} \\
+        --all_secis ${merged_secis_gff} \\
+        --filtered_secis ${filtered_secis} \\
         --output ORFsearch.result
     """
 }
--- submit.sh
+++ submit.sh
@@ -3,8 +3,8 @@
 #SBATCH --mem 8G
 #SBATCH -p genoa64
 #SBATCH --time 00:10:00
-#SBATCH --output=/no_backup/rg/ileahy/logs/nf_orfsearch_pfalciparum_%A.out  # log per job
-#SBATCH --error=/no_backup/rg/ileahy/logs/nf_orfsearch_pfalciparum_%A.err   # error log
+#SBATCH --output=/no_backup/rg/ileahy/logs/nf_orfsearch_Babesia_duncani_%A.out   # log per job
+#SBATCH --error=/no_backup/rg/ileahy/logs/nf_orfsearch_Babesia_duncani_%A.err   # error log
  
 # Configure bash
 set -e          # exit immediately on error
@@ -32,7 +32,7 @@ export NXF_JVM_ARGS="-Xms2g -Xmx5g"
 # $ sbatch submit_nf.sh nextflow/rnatoy -with-singularity
 #
 # will use "nextflow/rnatoy -with-singularity" as arguments
-nextflow run ~/git/gitlab/secis_independent/main.nf -params-file ~/git/gitlab/secis_independent/params.yaml -profile cluster -w /nfs/scratch01/rg/ileahy -resume
+nextflow run ~/git/gitlab/secis_independent/main.nf -params-file ~/git/gitlab/secis_independent/params.yaml -profile cluster -w /nfs/scratch01/rg/ileahy
  
 # Wait for the pipeline to finish
 echo "Waiting for ${pid}"
