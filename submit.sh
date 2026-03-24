0c010066b920d3e439e45d9d8f42903038b47bf5
Iseult Leahy <iseult.leahy@crg.eu>
2026-03-24 12:41:39 +0100

    test babesia on different Lyric

--- submit.sh
+++ submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --no-requeue
 #SBATCH --mem 8G
 #SBATCH -p genoa64
-#SBATCH --time 00:10:00
+#SBATCH --time 00:30:00
 #SBATCH --output=/no_backup/rg/ileahy/logs/nf_orfsearch_Babesia_duncani_%A.out   # log per job
 #SBATCH --error=/no_backup/rg/ileahy/logs/nf_orfsearch_Babesia_duncani_%A.err   # error log
  
