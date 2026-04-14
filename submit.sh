828fb737df0ad0b67d71ee18d0dceb70dcf2a850
Iseult Leahy <iseult.leahy@crg.eu>
2026-04-14 17:37:02 +0200

    increase time and resume when ready

--- submit.sh
+++ submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --no-requeue
 #SBATCH --mem 8G
 #SBATCH -p genoa64
-#SBATCH --time 01:00:00
+#SBATCH --time 03:00:00
 #SBATCH --output=/no_backup/rg/ileahy/logs/nf_orfsearch_Homo_sapiens_%A.out   # log per job
 #SBATCH --error=/no_backup/rg/ileahy/logs/nf_orfsearch_Homo_sapiens_%A.err   # error log
  
@@ -32,7 +32,7 @@ export NXF_JVM_ARGS="-Xms2g -Xmx5g"
 # $ sbatch submit_nf.sh nextflow/rnatoy -with-singularity
 #
 # will use "nextflow/rnatoy -with-singularity" as arguments
-nextflow run ~/git/gitlab/secis_independent/main.nf -params-file ~/git/gitlab/secis_independent/params.yaml -profile cluster -w /nfs/scratch01/rg/ileahy
+nextflow run ~/git/gitlab/secis_independent/main.nf -params-file ~/git/gitlab/secis_independent/params.yaml -profile cluster -w /nfs/scratch01/rg/ileahy -resume
  
 # Wait for the pipeline to finish
 echo "Waiting for ${pid}"
