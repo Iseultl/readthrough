af37b305b4368355225a4c108e154ab73530b77a
Iseult Leahy <iseult.leahy@crg.eu>
2026-08-11 10:01:06 +0100

    fix clean_gtf script to work on TOGA files

--- bin/clean_gtf.sh
+++ bin/clean_gtf.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-# Input parameters
+set -euo pipefail
+
 INPUT_FILE="$1"
 OUTPUT_FILE="${INPUT_FILE%.*}.cleaned.gtf"
 
-# Clean the GTF file to keep only transcript_name or fall back to transcript id if not present
-# Remove lines starting with #, lines with MT in the first column, and lines with gene in the third column
 awk -F'\t' '
 BEGIN {
     OFS="\t"
 }
 
 # Skip comments
-/^#/ { next }
+/^#/ {
+    next
+}
+
+# Require at least 9 GFF/GTF columns
+NF < 9 {
+    next
+}
 
 # Skip mitochondrial chromosome
-$1 == "MT" { next }
+$1 == "MT" || $1 == "chrM" || $1 == "M" {
+    next
+}
 
 # Skip gene features
-$3 == "gene" { next }
+$3 == "gene" {
+    next
+}
 
 {
     gene_id = ""
@@ -26,33 +36,100 @@ $3 == "gene" { next }
     gene_name = ""
     transcript_name = ""
 
+    # ---------------------------------------------------------
+    # GTF attributes
+    # ---------------------------------------------------------
+
     if (match($9, /gene_id "[^"]+"/))
-        gene_id = substr($9, RSTART, RLENGTH)
+        gene_id = substr($9, RSTART + 9, RLENGTH - 10)
 
     if (match($9, /transcript_id "[^"]+"/))
-        transcript_id = substr($9, RSTART, RLENGTH)
+        transcript_id = substr($9, RSTART + 14, RLENGTH - 15)
 
     if (match($9, /gene_name "[^"]+"/))
-        gene_name = substr($9, RSTART, RLENGTH)
+        gene_name = substr($9, RSTART + 10, RLENGTH - 11)
 
     if (match($9, /transcript_name "[^"]+"/))
-        transcript_name = substr($9, RSTART, RLENGTH)
+        transcript_name = substr($9, RSTART + 16, RLENGTH - 17)
 
-    # Require the two essential attributes
+
+    # ---------------------------------------------------------
+    # GFF3 attributes
+    # ---------------------------------------------------------
+
+    # ID=...
+    if (gene_id == "" && match($9, /(^|;)ID=[^;]+/)) {
+        value = substr($9, RSTART, RLENGTH)
+
+        sub(/^.*ID=/, "", value)
+
+        # A gene feature has ID=gene, while a transcript
+        # can also have ID=transcript.
+        if ($3 == "transcript" || \
+            $3 == "mRNA" || \
+            $3 == "mrna") {
+            transcript_id = value
+        }
+        else {
+            gene_id = value
+        }
+    }
+
+    # Parent=...
+    if (match($9, /(^|;)Parent=[^;]+/)) {
+        value = substr($9, RSTART, RLENGTH)
+        sub(/^.*Parent=/, "", value)
+
+        # Parent of a transcript is generally the gene.
+        if ($3 == "transcript" || \
+            $3 == "mRNA" || \
+            $3 == "mrna") {
+            gene_id = value
+        }
+    }
+
+
+    # ---------------------------------------------------------
+    # For exon/CDS records, Parent is usually the transcript
+    # ---------------------------------------------------------
+
+    if (transcript_id == "" && \
+        ($3 == "exon" || $3 == "CDS" || \
+         $3 == "start_codon" || $3 == "stop_codon")) {
+
+        if (match($9, /(^|;)Parent=[^;]+/)) {
+            value = substr($9, RSTART, RLENGTH)
+            sub(/^.*Parent=/, "", value)
+            transcript_id = value
+        }
+    }
+
+
+    # ---------------------------------------------------------
+    # Require gene_id and transcript_id
+    # ---------------------------------------------------------
+
     if (gene_id == "" || transcript_id == "")
         next
 
-    attrs = gene_id "; " transcript_id
+
+    # ---------------------------------------------------------
+    # Construct clean GTF attributes
+    # ---------------------------------------------------------
+
+    attrs = "gene_id \"" gene_id "\"; "
+    attrs = attrs "transcript_id \"" transcript_id "\";"
 
     if (gene_name != "")
-        attrs = attrs "; " gene_name
+        attrs = attrs " gene_name \"" gene_name "\";"
 
     if (transcript_name != "")
-        attrs = attrs "; " transcript_name
-
-    attrs = attrs ";"
+        attrs = attrs " transcript_name \"" transcript_name "\";"
 
     $9 = attrs
+
     print
 }
 ' "$INPUT_FILE" > "$OUTPUT_FILE"
+
+echo "Created: $OUTPUT_FILE"
