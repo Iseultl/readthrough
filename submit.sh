930aa78ec0bd7e71581b5281c3d7020ca999529f
Iseult Leahy <iseult.leahy@crg.eu>
2026-08-17 17:47:55 +0200

    update for next protist

--- submit.sh
+++ submit.sh
@@ -3,8 +3,8 @@
 #SBATCH --mem 8G
 #SBATCH -p genoa64
 #SBATCH --time 02:00:00
-#SBATCH --output=/no_backup/rg/ileahy/logs/nf_Eimeria_necatrix_%A.out   # log per job
-#SBATCH --error=/no_backup/rg/ileahy/logs/nf_Eimeria_necatrix_%A.err   # error log
+#SBATCH --output=/no_backup/rg/ileahy/logs/nf_Emiliania_huxleyi_CCMP1516_280463_%A.out   # log per job
+#SBATCH --error=/no_backup/rg/ileahy/logs/nf_Emiliania_huxleyi_CCMP1516_280463_%A.err   # error log
  
 # Configure bash
 set -e          # exit immediately on error
@@ -33,7 +33,7 @@ export NXF_JVM_ARGS="-Xms2g -Xmx5g"
 #
 # will use "nextflow/rnatoy -with-singularity" as arguments
 
-nextflow run main_protists.nf -params-file params.yaml -profile cluster --max_cpus 4 --max_memory 16GB -resume
+nextflow run main_protists.nf -params-file params.yaml -profile cluster --max_cpus 4 --max_memory 16GB
  
 # Wait for the pipeline to finish
 echo "Waiting for ${pid}"
